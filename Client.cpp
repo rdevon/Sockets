0a3ecb611b7e7c587fea4930813b7c7fa656c57c
Devon <devon@cs.unm.edu>
2012-10-29 13:22:58 -0600

    c

--- Client.cpp
+++ Client.cpp
@@ -21,7 +21,7 @@
 int port_number = 55555;
 std::string my_IP;
 pcrecpp::RE hello("HELLO I'M (.+)\n(.*)");
-pcrecpp::RE hello_back("HELLO (^,+), I'M (.+)\(.+)");
+pcrecpp::RE hello_back("HELLO ([^,\n]+), I'M (.+)\(.+)");
 pcrecpp::RE goodbye("GOODBYE (.+)\n(.*)");
 pcrecpp::RE generate_XYZ("GENERATE (\\d+) BYTES CALLED (\\w+)\n(.*)");
 pcrecpp::RE get_XYZ_from("GET (\\w+) FROM (.+)\n(.*)");
