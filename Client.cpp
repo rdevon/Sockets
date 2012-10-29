4a47665b789975bf0e1565bc43a8e464c4667c71
Devon <devon@cs.unm.edu>
2012-10-29 14:47:43 -0600

    c

--- Client.cpp
+++ Client.cpp
@@ -53,6 +53,7 @@ void enter_message(int socket_fd) {
    std::string message = out.str() + "\n";
    write(socket_fd, message.c_str(),message.length());
    bzero(buffer, strlen(buffer));
+   read(socket_fd, buffer, 255);
    std::cout << buffer << std::endl;
 }
 
