28d498d27ca1ee9e4f7684a7baa5fabce0e2ec1f
Devon <devon@cs.unm.edu>
2012-11-20 08:44:37 -0700

    push

--- Server.cpp
+++ Server.cpp
@@ -112,10 +112,10 @@ pcrecpp::RE punk("ARE YOU FEELING LUCKY, PUNK\?");
 pcrecpp::RE hello("HELLO(\\s+)I'M(\\s+)(.+)");
 pcrecpp::RE hello_back("HELLO(\\s+)(.+)(\\s+)I'M(\\s+)(.+)");
 pcrecpp::RE goodbye("GOODBYE(\\s+)(.+)");
-pcrecpp::RE generate_XYZ("GENERATE(\\s+)(\\d+)(\\s+)BYTES(\\s+)CALLED(\\s+)(\\d+)");
-pcrecpp::RE get_XYZ_from("GET(\\s+)(\\d+)(\\s+)FROM(\\s+)(.+)");
-pcrecpp::RE give_me_XYZ("GIVE(\\s+)ME(\\s+)(\\w+)");
-pcrecpp::RE XYZ_is("^(\\d+)(\\s+)IS(\\s+)(.+)");
+pcrecpp::RE generate_XYZ("GENERATE(\\s+)(\\.+)(\\s+)BYTES(\\s+)CALLED(\\s+)(\\.+)");
+pcrecpp::RE get_XYZ_from("GET(\\s+)(\\.+)(\\s+)FROM(\\s+)(.+)");
+pcrecpp::RE give_me_XYZ("GIVE(\\s+)ME(\\s+)(\\.+)");
+pcrecpp::RE XYZ_is("^(\\.+)(\\s+)IS(\\s+)(.+)");
 
 void error(const char *msg)
 {
